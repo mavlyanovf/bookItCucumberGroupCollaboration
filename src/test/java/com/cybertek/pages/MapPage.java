09779c5da2c4725cfb995622d40724c52c2806a9
Mansur-Bek <51763462+Mansur-Bek@users.noreply.github.com>
2019-08-06 17:32:55 -0500

    Update MapPage.java

--- src/test/java/com/cybertek/pages/MapPage.java
+++ src/test/java/com/cybertek/pages/MapPage.java
@@ -4,7 +4,7 @@ import com.cybertek.utilities.Driver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-
+//commented
 public class MapPage {
 
     public MapPage(){
